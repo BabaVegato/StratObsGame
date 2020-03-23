859bf245eb584a658c327c679c723f2649c9cc25
BabaVegato <45877172+BabaVegato@users.noreply.github.com>
2020-03-23 23:34:13 +0100

    Game can create server or connect to one

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
 import pygame
 import client
+import server
 import threading
+import socket
 
 pygame.init()
 
 host = "192.168.1.66"
 port = 5555
+is_waiting_for_connexion = False
 
 #Dimensions écran
 winWidth = 500
@@ -31,34 +34,77 @@ class button(object):
         pygame.draw.rect(win, (0,0,0), self.hitbox)
 
 def redrawWindow():
-    win.fill((255, 255, 255))
-    btnTest.draw(win)
-    pygame.display.update()
+    if not is_waiting_for_connexion :
+        win.fill((255, 255, 255))
+        btnTest1.draw(win)
+        btnTest2.draw(win)
+        pygame.display.update()
 
-btnTest = button(200, 200, 100, 100, "btnTest")
+    if is_waiting_for_connexion :
+        win.fill((255, 255, 255))
+        # pick a font you have and set its size
+        myfont = pygame.font.SysFont("Comic Sans MS", 30)
+        # apply it to text on a label
+        label = myfont.render("Python and Pygame are Fun!", 1, yellow)
+        # put the label object on the screen at point x=100, y=100
+        win.blit(label, (100, 100))
+        pygame.display.update()
+
+
+btnTest1 = button(200, 200, 100, 100, "btnTest1")
+btnTest2 = button(400, 200, 100, 100, "btnTest2")
+
+def launch_server(threads, is_waiting_for_connexion):
+    if not is_waiting_for_connexion :
+        is_waiting_for_connexion = True
+        print(is_waiting_for_connexion)
+        serv = server.Server()
+        serv.create_server(host, port)
+        wait = threading.Thread(target=serv.wait_for_a_connection)
+        threads.append(wait)
+        wait.start()
+        is_waiting_for_connexion = True
+    else :
+        stop = threading.Thread(target=serv.socket.shutdown)
+        print("shutdown")
+        stop.start()
 
 def main():
+    is_waiting_for_connexion = False
+    threads = [threading.Thread]
     run = True
+    serv = None
     mouse = "" #l'objet que pointe la souris
     clic = False #Limiter à un clic
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("yes") #??????????????????????????????
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((host, port))
                 run = False
+                quit()
     
         #Test position souris
         xMouse, yMouse = pygame.mouse.get_pos() #Position de la souris
-        if xMouse > btnTest.x and xMouse < btnTest.xMax and yMouse > btnTest.y and yMouse < btnTest.yMax :
-            mouse = btnTest.id
+        if xMouse > btnTest1.x and xMouse < btnTest1.xMax and yMouse > btnTest1.y and yMouse < btnTest1.yMax :
+            mouse = btnTest1.id
+        elif xMouse > btnTest2.x and xMouse < btnTest2.xMax and yMouse > btnTest2.y and yMouse < btnTest2.yMax :
+            mouse = btnTest2.id
         else :
             mouse = ""
 
         #Clic bouton
         if pygame.mouse.get_pressed()[0]: #Si clic gauche
-            if mouse == btnTest.id and not(clic):
+            if mouse == btnTest1.id and not(clic):
                 clic = True
-                print("Bouton")
+                print("Bouton server")
+                launch_server(threads, is_waiting_for_connexion)
+
+            if mouse == btnTest2.id and not(clic):
+                clic = True
+                print("Bouton client")
                 cli = client.create_client(host, port)
                 client.send_obj(cli, d)
         else :
--- server.py
+++ server.py
@@ -1,29 +1,45 @@
 import socket
-from _thread import *
-import sys
+import threading
 import pickle
 import select
 
-HEADERSIZE = 10
-
 d = {1: "hey", 2: "there"}
 
-host = "192.168.1.66"
-port = 5555
+class Server:
+    def __init__(self):
+        self.host = "192.168.1.66"
+        self.port = 5555
+        self.socket = None
+        self.running = False
+        self.conn = None
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen()
+    def create_server(self, host, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((host, port))
+        self.socket.listen()
+        self.running = True
+        self.conn_addr = None
+        print("Listening on port 5555 ...")
 
-print("Listening on port 5555 ...")
-while True:
-    conn, addr = s.accept()
-    print(f"Connection from {addr} has been established !")
-    msg = pickle.dumps(d)
-    conn.send(msg)
-    print("Sent an object : ", d)
-    received = conn.recv(2048)
-    obj = pickle.loads(received)
-    print("Received an object : ", obj)
+    def wait_for_a_connection(self):
+        while self.conn_addr is None :
+            self.conn, self.conn_addr = self.socket.accept()
+            print(f"Connection from {self.conn_addr} has been established !")
 
+    def stop_accept(self):
+        #stop the thread
+        self.running = False
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+
+    def send_obj(conn, obj):
+        msg = pickle.dumps(obj)
+        self.conn.send(msg)
+        print("Sent an object : ", obj)
+        
+
+    def wait_for_obj(conn):
+        received = conn.recv(2048)
+        obj = pickle.loads(received)
+        print("Received an object : ", obj)
 
