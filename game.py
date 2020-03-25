fb3cc908c8ff403c3d8c2dbf182ed836c7955c55
Axel Farrugia <axelfar2@gmail.com>
2020-03-25 13:21:41 +0100

    grid
    Commencé à faire la grille

--- game.py
+++ game.py
@@ -21,12 +21,37 @@ winWidth = 1300
 winHeight = 750
 d = {1: winWidth, 2: winHeight}
 
+#Grille
+grid = [[0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]]
+
 win = pygame.display.set_mode((winWidth, winHeight))
 
 pygame.display.set_caption("First Game")
 clock = pygame.time.Clock()
 
-font = pygame.font.Font(r"C:\Users\Baptiste\Desktop\Eyes\StratObsGame\media\BlackOpsOne-Regular.ttf",30)
+class case(object):
+    def __init__(self, x, y, width, height, nbX, nbY):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.gap = 3
+        self.offsetX = (winWidth-(nbX*self.width+(nbX-1)*self.gap))//2 #Centrer la grille
+        self.offsetY = (winHeight-(nbY*self.height+(nbY-1)*self.gap))//2
+    def draw(self, win):
+        hitbox = (self.x, self.y, self.width, self.height)
+        pygame.draw.rect(win, (0,0,0), hitbox)
 
 class button(object):
     def __init__(self, x, y, width, height, id, text):
@@ -45,6 +70,26 @@ class button(object):
         textSurface = fontBtn.render(self.text, False, (255,255,255))
         win.blit(textSurface, self.posText)
 
+#Boutons :
+butHeight1 = 100
+butWidth1 = 300
+btnCreate = button(int(winWidth/2-butWidth1/2), int(winHeight/3), butWidth1, butHeight1, "btnCreate", "Créer une partie")
+btnJoin = button(int(winWidth/2-butWidth1/2), int(winHeight*2/3), butWidth1, butHeight1, "btnJoin", "Rejoindre une partie")
+
+#Cases :
+case = case(0,0,50,50,9,11)
+
+def pass_time(seconds):
+    time.sleep(seconds) #Y'avait autre chose mais jsplus quoi et ça marche alors bon........
+
+def displayGrid():
+    for i in range(11):
+        for j in range(9):
+            case.x = case.offsetX + j*(case.width + case.gap)
+            case.y = case.offsetY + i*(case.height + case.gap)
+            if i==0 and j==0 :
+                print(case.x,case.y)
+            case.draw(win)
 
 def redrawWindow(state):
     if state == "entry" :
@@ -73,14 +118,9 @@ def redrawWindow(state):
     elif state =="map creation":
         win.fill((240, 240, 240))
         text = font.render("Create the map", True, (0, 128, 0))
-        win.blit(text,(winWidth//2 - text.get_width() // 2, winHeight//4 - text.get_height() // 2))
+        win.blit(text,(winWidth//2 - text.get_width() // 2, winHeight//20 - text.get_height() // 2))
+        displayGrid()
         pygame.display.update()
-
-#Boutons :
-butHeight1 = 100
-butWidth1 = 300
-btnCreate = button(int(winWidth/2-butWidth1/2), int(winHeight/3), butWidth1, butHeight1, "btnCreate", "Créer une partie")
-btnJoin = button(int(winWidth/2-butWidth1/2), int(winHeight*2/3), butWidth1, butHeight1, "btnJoin", "Rejoindre une partie")
 
 def launch_server(state):
     serv = server.Server()
@@ -95,7 +135,7 @@ def adapt_to_server(cli, state):
     return state
 
 def main():
-    state = "entry"
+    state = "map creation"
     run = True
     serv = None
     cli = None
